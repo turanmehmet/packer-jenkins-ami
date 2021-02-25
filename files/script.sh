f27f28fc0a088586d553da2225dc0b8ece32041c
Mehmet <mehmet.72.turan@gmail.com>
2021-02-25 20:03:18 +0000

    fixing the script

--- files/script.sh
+++ files/script.sh
@@ -1,8 +1,11 @@
-"sudo yum update -y",
-"sudo yum install wget -y",
-"sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-"sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-"sudo yum install   jenkins-{{user `jenkins_version`}} -y",
-"sudo yum install   java-{{user `java_version`}}-openjdk-devel -y",
-"sudo systemctl start jenkins",
-"sudo systemctl status jenkins"
+#!/bin/bash
+echo "Get Updates"
+sudo yum update -y
+sudo yum install wget -y
+sudo yum install   java-{{user `java_version`}}-openjdk-devel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install   jenkins-{{user `jenkins_version`}} -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
