3cf675b8df3ef0f1581db6f68853e9147eacee98
Mehmet <mehmet.72.turan@gmail.com>
2021-02-25 20:35:24 +0000

    updated

--- files/script.sh
+++ files/script.sh
@@ -2,10 +2,11 @@
 echo "Get Updates"
 sudo yum update -y
 sudo yum install wget -y
-sudo yum install   java-{{user `java_version`}}-openjdk-devel -y
+# sudo yum install   java-{{user `java_version`}}-openjdk-devel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install   jenkins-{{user `jenkins_version`}} -y
+sudo yum install -y jenkins-${JENKINS_VERSION}
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+#Get auth password from jenkins master
+echo "authpwd="$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
