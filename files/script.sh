7c975fd8f4980bf4c809c24ef5d253c00b0f5bb4
Mehmet <mehmet.72.turan@gmail.com>
2021-02-26 03:26:26 +0000

    fixed script version issue

--- files/script.sh
+++ files/script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "Get Updates"
 sudo yum update -y
 sudo yum install wget -y
-# sudo yum install   java-{{user `java_version`}}-openjdk-devel -y
+sudo yum install   java-$(grep java_version files/variables.json | awk '{print $NF}' | sed 's/"//g')-openjdk-devel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install -y jenkins-${JENKINS_VERSION}
+sudo yum install jenkins-$(grep jenkins_version files/variables.json | awk '{print $NF}' | sed 's/,//g' | sed 's/"//g')
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 #Get auth password from jenkins master
