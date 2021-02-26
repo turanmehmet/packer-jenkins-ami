53a1ed3f2e8b2beb59cf3bd8c3d01317e8ea8515
Mehmet <mehmet.72.turan@gmail.com>
2021-02-26 03:55:16 +0000

    added variable file on the remote machine

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # parker-build-jenkins-ami
 Building jenkins aws ami with packer
+Run the following command
+
+`packer build -var-file=files/variables.json  jenkins.json`
--- files/script.sh
+++ files/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install wget -y
-sudo yum install   java-$(grep java_version files/variables.json | awk '{print $NF}' | sed 's/"//g')-openjdk-devel -y
+sudo yum install   java-$(grep java_version /tmp/variables.json | awk '{print $NF}' | sed 's/"//g')-openjdk-devel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install jenkins-$(grep jenkins_version files/variables.json | awk '{print $NF}' | sed 's/,//g' | sed 's/"//g')
+sudo yum install -y jenkins-$(grep jenkins_version /tmp/variables.json | awk '{print $NF}' | sed 's/,//g' | sed 's/"//g')
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 #Get auth password from jenkins master
